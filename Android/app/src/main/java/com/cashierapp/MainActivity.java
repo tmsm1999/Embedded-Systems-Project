d6feab628596c650a41778de76c009aa26d2c283
Yosbi Alves Saenz <yosbialves@icloud.com>
2021-05-23 14:03:30 +0100

    fixed installation scripts for raspberry

--- Android/app/src/main/java/com/cashierapp/MainActivity.java
+++ Android/app/src/main/java/com/cashierapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.cashierapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startCashiers(View view) {
+        Intent intent = new Intent(this, Cashiers.class);
+        startActivity(intent);
     }
 }
